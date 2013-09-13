abd317764ea83820fd0fd070f28dec83306d9761
Ricky <ricky@dius.com.au>
2013-09-13 11:20:17 +1000

    removing from branch

--- src/main/java/com/github/codingricky/runkeeperclient/Callback.java
+++
@@ -1,6 +0,0 @@
-package com.github.codingricky.runkeeperclient;
-
-public interface Callback<T> {
-
-    void success(T result, String json);
-}
--- src/main/java/com/github/codingricky/runkeeperclient/model/WeightFeed.java
+++
@@ -1,6 +0,0 @@
-package com.github.codingricky.runkeeperclient.model;
-
-public class WeightFeed {
-    private WeightFeedItem[] items;
-    private int size;
-}
