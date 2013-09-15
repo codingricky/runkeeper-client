6e2ef50fe6f0590b6aee493a902efccab62fa418
Ricky <ricky@dius.com.au>
2013-09-15 12:33:08 +1000

    put the source/target as 1.6

---
+++ src/main/java/com/github/codingricky/runkeeperclient/model/TeamMember.java
@@ -0,0 +1,9 @@
+package com.github.codingricky.runkeeperclient.model;
+
+public class TeamMember {
+
+    private long userID;
+    private String status;
+    private String name;
+    private String profile;
+}
